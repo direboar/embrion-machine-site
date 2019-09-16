04e947c7e839dbec0fb64ae5d67bba0d6779e807
direboar <minokuba@example.com>
2019-09-16 15:53:12 +0900

    接続先DB変更対応のテスト

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,8 +1,8 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  HEADER_DB : '"embriomachine/header"',
-  DETAIL_DB : '"embriomachine/detail"',
-  FILE_UPLOAD_DIR : '"embriomachine/files"'
+  HEADER_DB : process.env.npm_package_HEADER_DB ? process.env.npm_package_HEADER_DB : '"embriomachine/header"',
+  DETAIL_DB : process.env.npm_package_DETAIL_DB ? process.env.npm_package_DETAIL_DB : '"embriomachine/detail"',
+  FILE_UPLOAD_DIR : process.env.npm_package_FILE_UPLOAD_DIR ? process.env.npm_package_FILE_UPLOAD_DIR : '"embriomachine/files"'
 }
 
