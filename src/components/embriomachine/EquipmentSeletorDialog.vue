9bbbb43ed9bcdc22d0b52c28fa6611cc1d92c86c
direboar <minokuba@example.com>
2019-09-19 15:30:41 +0900

    武装の選択画面改修中

--- src/components/embriomachine/EquipmentSeletorDialog.vue
+++ src/components/embriomachine/EquipmentSeletorDialog.vue
@@ -6,236 +6,263 @@
       max-width="1000"
       :fullscreen="isMd"
     >
-      <v-card>
-        <v-layout
-          row
-          wrap
-        >
-          <v-flex
-            sm3
-            xs6
+      <v-layout row>
+        <v-card>
+          <v-container
+            fruid
+            grid-list-md
           >
-            <v-radio-group
-              label="ルール"
-              v-model="edition"
-              :row="!isXs"
-              v-if="editMode"
+            <v-layout
+              row
+              wrap
+              justify-center
             >
-              <v-radio
-                v-for="edition in editions"
-                :label="edition"
-                :value="edition"
-                :key="edition"
-                :disabled="!editMode"
-                class="body-1"
-                color="light-green lighten-1"
-              ></v-radio>
-            </v-radio-group>
-            <v-radio-group
-              label="ランク"
-              v-model="rank"
-              :row="!isXs"
-              v-if="editMode"
-            >
-              <v-radio
-                v-for="rank in ranks"
-                :label="rank"
-                :value="rank"
-                :key="rank"
-                :disabled="!editMode"
-                class="body-1"
-                color="light-green lighten-1"
-              ></v-radio>
-            </v-radio-group>
-            <!--FIXME コンポーネント化-->
-            <v-toolbar
-              floating
-              dense
-              v-if="isXs"
-            >
-              <v-select
-                v-if="editMode"
-                :items="itemcounts"
-                v-model="itemcount"
-                label="装備数"
-                item-value="text"
-              ></v-select>
-              <v-tooltip>
-                <v-btn
-                  slot="activator"
-                  icon
-                  @click.native="select"
-                  :disabled="!editMode"
-                >
-                  <v-icon>fas fa-hand-pointer</v-icon>
-                </v-btn>
-                <span>装備を選択します。</span>
-              </v-tooltip>
-              <v-tooltip>
-                <v-btn
-                  slot="activator"
-                  icon
-                  @click.native="closeDialog"
-                >
-                  <v-icon>fas fa-backward</v-icon>
-                </v-btn>
-                <span>機体作成・編集画面に戻ります。</span>
-              </v-tooltip>
-            </v-toolbar>
-          </v-flex>
-          <v-flex
-            sm9
-            xs6
-          >
-            <v-radio-group
-              label="種類"
-              v-model="type"
-              :row="!isXs"
-              v-if="editMode"
-              class="body-1"
-            >
-              <v-radio
-                v-for="type in types"
-                :label="type"
-                :value="type"
-                :key="type"
-                :disabled="!editMode"
-                color="brown lighten-2"
-              ></v-radio>
-            </v-radio-group>
-          </v-flex>
-          <v-flex
-            sm4
-            xs6
-          >
-            <v-radio-group
-              label="装備"
-              v-model="selectedEquipmentName"
-              :mandatory="false"
-            >
-              <v-radio
-                v-for="equipment in equipments"
-                :label="formatEquipmentName(equipment)"
-                :value="equipment.name"
-                :key="equipment.name"
-                :disabled="!editMode"
-                class="caption"
-              ></v-radio>
-            </v-radio-group>
-          </v-flex>
-          <v-flex
-            sm4
-            xs3
-          >
-            <v-list :dense="isXs">
-              <v-list-tile>
+              <v-flex
+                sm1
+                xs6
+              />
+              <v-flex
+                sm2
+                xs6
+              >
                 <v-select
+                  dense
+                  label="Select"
+                  :items="editions"
+                  v-model="selectedEditions"
+                  max-height="400"
+                  hint="ルール"
+                  persistent-hint
+                  multiple
+                  :disabled="!editMode"
+                ></v-select>
+              </v-flex>
+              <v-flex
+                sm2
+                xs6
+              >
+                <v-select
+                  dense
+                  label="Select"
+                  :items="ranks"
+                  v-model="selectedRanks"
+                  max-height="400"
+                  hint="ランク"
+                  persistent-hint
+                  multiple
+                  :disabled="!editMode"
+                ></v-select>
+              </v-flex>
+              <v-flex
+                sm2
+                xs6
+              >
+                <v-select
+                  dense
+                  label="Select"
+                  :items="types"
+                  v-model="selectedTypes"
+                  max-height="400"
+                  hint="種類"
+                  persistent-hint
+                  multiple
+                  :disabled="!editMode"
+                ></v-select>
+              </v-flex>
+              <v-flex
+                sm4
+                xs6
+              >
+                <v-text-field
                   v-if="editMode&&!isXs"
+                  v-model="searchEquipmentName"
+                  label="装備名（入力した名称で絞り込みます）"
+                  item-value="text"
+                  :disabled="!editMode"
+                ></v-text-field>
+              </v-flex>
+              <v-flex
+                sm1
+                xs6
+              />
+              <!--FIXME コンポーネント化-->
+              <v-toolbar
+                floating
+                dense
+                v-if="isXs"
+              >
+                <v-select
+                  v-if="editMode"
                   :items="itemcounts"
                   v-model="itemcount"
                   label="装備数"
                   item-value="text"
                 ></v-select>
-              </v-list-tile>
-              <v-list-tile>
-              </v-list-tile>
-              <v-list-tile>
-                <v-list-tile-content :class="contentClass">種別／ランク</v-list-tile-content>
-              </v-list-tile>
-              <v-divider />
-              <v-list-tile>
-                <v-list-tile-content :class="contentClass">射程</v-list-tile-content>
-              </v-list-tile>
-              <v-divider />
-              <v-list-tile>
-                <v-list-tile-content :class="contentClass">ダメージ</v-list-tile-content>
-              </v-list-tile>
-              <v-divider />
-              <v-list-tile>
-                <v-list-tile-content :class="contentClass">搭載可能部位</v-list-tile-content>
-              </v-list-tile>
-              <v-divider />
-              <v-list-tile>
-                <v-list-tile-content :class="contentClass">最低枚数</v-list-tile-content>
-              </v-list-tile>
-              <v-divider />
-              <v-list-tile>
-                <v-list-tile-content :class="contentClass">上限枚数</v-list-tile-content>
-              </v-list-tile>
-              <v-divider />
-            </v-list>
-          </v-flex>
-          <v-flex
-            sm4
-            xs3
-          >
-            <v-list :dense="isXs">
-              <v-list-tile>
-                <v-toolbar
-                  floating
-                  v-if="!isXs"
+                <v-tooltip>
+                  <v-btn
+                    slot="activator"
+                    icon
+                    @click.native="select"
+                    :disabled="!editMode"
+                  >
+                    <v-icon>fas fa-hand-pointer</v-icon>
+                  </v-btn>
+                  <span>装備を選択します。</span>
+                </v-tooltip>
+                <v-tooltip>
+                  <v-btn
+                    slot="activator"
+                    icon
+                    @click.native="closeDialog"
+                  >
+                    <v-icon>fas fa-backward</v-icon>
+                  </v-btn>
+                  <span>機体作成・編集画面に戻ります。</span>
+                </v-tooltip>
+              </v-toolbar>
+              <v-flex
+                sm9
+                xs6
+              />
+              <v-flex
+                sm4
+                xs6
+              >
+                <v-layout
+                  style="max-height: 600px"
+                  class="scroll-y"
                 >
-                  <v-tooltip>
-                    <v-btn
-                      slot="activator"
-                      icon
-                      @click.native="select"
+                  <v-radio-group
+                    label="装備"
+                    v-model="selectedEquipmentName"
+                    :mandatory="false"
+                  >
+                    <v-radio
+                      v-for="equipment in equipments"
+                      :label="formatEquipmentName(equipment)"
+                      :value="equipment.name"
+                      :key="equipment.name"
                       :disabled="!editMode"
+                      class="caption"
+                    ></v-radio>
+                  </v-radio-group>
+                </v-layout>
+              </v-flex>
+              <v-flex
+                sm4
+                xs3
+              >
+                <v-list :dense="isXs">
+                  <v-list-tile>
+                    <v-select
+                      v-if="editMode&&!isXs"
+                      :items="itemcounts"
+                      v-model="itemcount"
+                      label="装備数"
+                      item-value="text"
+                    ></v-select>
+                  </v-list-tile>
+                  <v-list-tile>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-content :class="contentClass">種別／ランク</v-list-tile-content>
+                  </v-list-tile>
+                  <v-divider />
+                  <v-list-tile>
+                    <v-list-tile-content :class="contentClass">射程</v-list-tile-content>
+                  </v-list-tile>
+                  <v-divider />
+                  <v-list-tile>
+                    <v-list-tile-content :class="contentClass">ダメージ</v-list-tile-content>
+                  </v-list-tile>
+                  <v-divider />
+                  <v-list-tile>
+                    <v-list-tile-content :class="contentClass">搭載可能部位</v-list-tile-content>
+                  </v-list-tile>
+                  <v-divider />
+                  <v-list-tile>
+                    <v-list-tile-content :class="contentClass">最低枚数</v-list-tile-content>
+                  </v-list-tile>
+                  <v-divider />
+                  <v-list-tile>
+                    <v-list-tile-content :class="contentClass">上限枚数</v-list-tile-content>
+                  </v-list-tile>
+                  <v-divider />
+                </v-list>
+              </v-flex>
+              <v-flex
+                sm4
+                xs3
+              >
+                <v-list :dense="isXs">
+                  <v-list-tile>
+                    <v-toolbar
+                      floating
+                      v-if="!isXs"
                     >
-                      <v-icon>fas fa-hand-pointer</v-icon>
-                    </v-btn>
-                    <span>装備を選択します。</span>
-                  </v-tooltip>
-                  <v-tooltip>
-                    <v-btn
-                      slot="activator"
-                      icon
-                      @click.native="closeDialog"
-                    >
-                      <v-icon>fas fa-backward</v-icon>
-                    </v-btn>
-                    <span>機体作成・編集画面に戻ります。</span>
-                  </v-tooltip>
-                </v-toolbar>
-              </v-list-tile>
-              <v-list-tile>
-              </v-list-tile>
-              <v-list-tile>
-                <v-list-tile-content :class="contentClass">{{equipment.type}}（{{equipment.rank}}）</v-list-tile-content>
-              </v-list-tile>
-              <v-divider />
-              <v-list-tile>
-                <v-list-tile-content :class="contentClass">{{equipment.range}}</v-list-tile-content>
-              </v-list-tile>
-              <v-divider />
-              <v-list-tile>
-                <v-list-tile-content :class="contentClass">{{formatDamage(equipment)}}</v-list-tile-content>
-              </v-list-tile>
-              <v-divider />
-              <v-list-tile>
-                <v-list-tile-content :class="contentClass">{{equipment.mountPosition}}</v-list-tile-content>
-              </v-list-tile>
-              <v-divider />
-              <v-list-tile>
-                <v-list-tile-content :class="contentClass">{{equipment.formatMinLimit}}</v-list-tile-content>
-              </v-list-tile>
-              <v-divider />
-              <v-list-tile>
-                <v-list-tile-content :class="contentClass">{{equipment.maxLimit}}</v-list-tile-content>
-              </v-list-tile>
-              <v-divider />
-            </v-list>
-          </v-flex>
-          <v-flex xs12>
-            <v-card-text class="body-1">
-              <span
-                style="white-space: pre-wrap;display: inline-block;height:30px;"
-                v-html="equipment.effect"
-              ></span>
-            </v-card-text>
-          </v-flex>
-        </v-layout>
-      </v-card>
+                      <v-tooltip>
+                        <v-btn
+                          slot="activator"
+                          icon
+                          @click.native="select"
+                          :disabled="!editMode"
+                        >
+                          <v-icon>fas fa-hand-pointer</v-icon>
+                        </v-btn>
+                        <span>装備を選択します。</span>
+                      </v-tooltip>
+                      <v-tooltip>
+                        <v-btn
+                          slot="activator"
+                          icon
+                          @click.native="closeDialog"
+                        >
+                          <v-icon>fas fa-backward</v-icon>
+                        </v-btn>
+                        <span>機体作成・編集画面に戻ります。</span>
+                      </v-tooltip>
+                    </v-toolbar>
+                  </v-list-tile>
+                  <v-list-tile>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-content :class="contentClass">{{equipment.type}}（{{equipment.rank}}）</v-list-tile-content>
+                  </v-list-tile>
+                  <v-divider />
+                  <v-list-tile>
+                    <v-list-tile-content :class="contentClass">{{equipment.range}}</v-list-tile-content>
+                  </v-list-tile>
+                  <v-divider />
+                  <v-list-tile>
+                    <v-list-tile-content :class="contentClass">{{formatDamage(equipment)}}</v-list-tile-content>
+                  </v-list-tile>
+                  <v-divider />
+                  <v-list-tile>
+                    <v-list-tile-content :class="contentClass">{{equipment.mountPosition}}</v-list-tile-content>
+                  </v-list-tile>
+                  <v-divider />
+                  <v-list-tile>
+                    <v-list-tile-content :class="contentClass">{{equipment.formatMinLimit}}</v-list-tile-content>
+                  </v-list-tile>
+                  <v-divider />
+                  <v-list-tile>
+                    <v-list-tile-content :class="contentClass">{{equipment.maxLimit}}</v-list-tile-content>
+                  </v-list-tile>
+                  <v-divider />
+                </v-list>
+              </v-flex>
+              <v-flex xs12>
+                <v-card-text class="body-1">
+                  <span
+                    style="white-space: pre-wrap;display: inline-block;height:30px;"
+                    v-html="equipment.effect"
+                  ></span>
+                </v-card-text>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-layout>
     </v-dialog>
   </div>
 </template>
@@ -284,13 +311,22 @@ export default {
         Equipment.TYPE_IDOU,
         Equipment.TYPE_SONOTA
       ],
-      type: Equipment.TYPE_SHAGEKI,
+      selectedTypes: [Equipment.TYPE_SHAGEKI],
       ranks: ["S", "A", "B"],
-      rank: "B",
+      selectedRanks: ["B"],
       itemcounts: [1, 2, 3],
       itemcount: 1,
       editions: ["基本", "玉座"],
-      edition: "基本"
+      selectedEditions: ["基本"],
+      searchEquipmentName: "",
+      headers: [
+        {
+          text: "武装名",
+          align: "left",
+          sortable: false,
+          value: "name"
+        }
+      ]
     };
   },
 
@@ -298,9 +334,9 @@ export default {
     targetEquipment: function(val) {
       if (val && val.name) {
         this.selectedEquipmentName = val.name;
-        this.type = val.type;
-        this.rank = val.rank;
-        this.edition = val.edition;
+        this.selectetTypes = [val.type];
+        this.selectedRanks = [val.rank];
+        this.selecteEditions = [val.edition];
       }
     },
     equipment: function(val) {
@@ -316,22 +352,42 @@ export default {
   computed: {
     equipments() {
       return Equipment.getEquipments().filter(equipment => {
-        if (this.type !== null) {
-          if (!equipment.isTypeOf(this.type)) {
+        let retVal = true;
+        retVal = this.selectedTypes.reduce((accumurator, type) => {
+          if (!accumurator && !equipment.isTypeOf(type)) {
             return false;
+          } else {
+            return true;
           }
-          if (equipment.rank !== this.rank) {
-            return false;
-          }
-          if (equipment.edition !== this.edition) {
-            return false;
-          }
+        }, false);
+        retVal =
+          retVal &&
+          this.selectedRanks.reduce((accumurator, rank) => {
+            if (!accumurator && equipment.rank !== rank) {
+              return false;
+            } else {
+              return true;
+            }
+          }, false);
+        retVal =
+          retVal &&
+          this.selectedEditions.reduce((accumurator, edition) => {
+            if (!accumurator && equipment.edition !== edition) {
+              return false;
+            } else {
+              return true;
+            }
+          }, false);
+
+        if (this.searchEquipmentName !== "") {
+          retVal = retVal && equipment.name.includes(this.searchEquipmentName);
         }
-        if (this.targetPosition === null) {
-          return true;
-        } else {
-          return equipment.canEquip(this.targetPosition);
+
+        if (!equipment.canEquip(this.targetPosition)) {
+          retVal = false;
         }
+        // console.log(equipment.name + " = " + retVal);
+        return retVal;
       });
     },
 
